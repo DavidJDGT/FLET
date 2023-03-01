06dc276d66de471f6d2f17716522a004d4034b4e
DavidJDGT <90360699+DavidJDGT@users.noreply.github.com>
2023-03-01 12:28:57 -0500

    version 1.2

--- ToDo/main.py
+++ ToDo/main.py
@@ -1,34 +1,39 @@
 import flet as ft
 
-def main (page: ft.Page):
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
+        self.tasks = ft.Column()
+
+        # application's root control (i.e. "view") containing all other controls
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
 
 
-    def add_click(e):
-        page.add(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        page.update()
-
-
-    new_task = ft.TextField(hint_text="What's needs to be done? ", expand=True)
-
-    tasks_view = ft.Column()
-    
-    view = ft.Column(
-        width=600,
-        controls=[
-            ft.Row(
-                controls=[
-                    new_task,
-                    ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_click),
-                ],
-            ),
-            tasks_view,
-        ],
-    )
-
+def main(page: ft.Page):
+    page.title = "ToDo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
 
+    # create application instance
+    todo = TodoApp()
 
+    # add application's root control to the page
+    page.add(todo)
 
 ft.app(target=main)
